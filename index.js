afa93da9e92722c5cc6bf0122e52a17b3f2cac73
Leo <leo5j472421@gmail.com>
2018-03-05 15:35:48 +0800

    GJ

--- index.js
+++ index.js
@@ -27,6 +27,181 @@ function getTickerData(pair) {
 
 
 
+poloniex = function () {
+    socket = function () {
+        var self = this;
+        this.tick = {};
+        this.trade = {};
+        this.ids = {};
+        this.cps = {};
+        this.marketChannel = [];
+        this.market = ['USDT_BTC', 'USDT_ETH' , 'USDT_LTC' , 'USDT_XRP' ];
+        for (var cp in this.market)
+            this.trade[this.market[cp]] = {'asks': {}, 'bids': {}}
+
+        function tickEvent(data) {
+            cp = self.ids[data[0]];
+            //console.log(self.tick.USDT_ETH);
+            try {
+                self.tick[cp].price = data[1];
+                self.tick[cp].volume = data[5];
+                self.tick[cp].change = data[4];
+                //console.log(self.tick);
+            } catch (err) {
+                // tickInit is not finished yet
+                console.log(err);
+            }
+            //if (cp === 'USDT_ETH')
+            //console.log(self.tick[cp].price)
+
+        }
+
+
+        function getTickerData(e) {
+            return rp('https://poloniex.com/public?command=returnTicker').then(data => {
+                data = JSON.parse(data);
+                for (let d in data) {
+                    self.ids[data[d]['id']] = d;
+                    self.cps[d] = data[d]['id'];
+                    self.tick[d] = {
+                        'price': data[d].last,
+                        'volume': data[d].baseVolume,
+                        'change': data[d].percentChange,
+                        'high': data[d].high24hr,
+                        'low' : data[d].low24hr
+                    };
+                }
+                return e.target
+            });
+        }
+
+        function tickInit(e) {
+            return new Promise(function (resolve, reject) {
+                request('https://poloniex.com/public?command=returnTicker', function (error, response, body) {
+                    if (!error && response.statusCode === 200) {
+                        data = JSON.parse(body);
+                        for (let d in data) {
+                            self.ids[data[d]['id']] = d;
+                            self.cps[d] = data[d]['id'];
+                            self.tick[d] = {
+                                'price': data[d].last,
+                                'volume': data[d].baseVolume,
+                                'change': data[d].percentChange,
+                                'high': data[d].high24hr,
+                                'low' : data[d].low24hr
+                            };
+                        }
+
+                        console.log('end for')
+                    }
+                });
+                resolve(e.target);
+            });
+
+        }
+
+        function tradeEvent(datas, cp) {
+            for (let i in datas) {
+                var data = datas[i];
+                if (data[0] === 'o') {
+                    side = data[1] ? 'bids' : 'asks';
+                    if (data[3] === '0.00000000') {
+                        delete self.trade[cp][side][data[2]];
+                    }
+                    else
+                        self.trade[cp][side][data[2]] = data[3]
+
+                }
+            }
+
+            n = Object.keys(self.trade[cp].asks).map(parseFloat);
+            //console.log(Math.min(...n));
+
+        }
+
+
+        function tradeInit(data, cp) {
+            for (var a in [0, 1]) {
+                for (let rate in data[a]) { // 0 asks 1 bids
+                    if (a == 1)  // bids
+                        self.trade[cp].bids[rate] = data[a][rate];
+                    else
+                        self.trade[cp].asks[rate] = data[a][rate];
+                }
+            }
+        }
+
+
+        function webSockets_subscribe(conn) {
+            console.log('subscribe');
+            if (conn.readyState === 1) {
+                var params = {command: "subscribe", channel: 1002};
+                conn.send(JSON.stringify(params));
+                for (let a in self.market) {
+                    //console.log();
+                    conn.send(JSON.stringify({command: "subscribe", channel: self.market[a]}));
+                }
+            }
+        }
+
+        this.start = function () {
+
+            const mySocket = new WebSocket("wss://api2.poloniex.com");
+
+            mySocket.onopen = function (e) {
+
+
+                getTickerData(e).then(webSockets_subscribe);
+
+            };
+
+
+            mySocket.onmessage = function (e) {
+                data = JSON.parse(e.data);
+                //console.log(data);
+                channel = data[0];
+                var cp = self.ids[channel];
+                if (channel === 1002) {
+                    if (data[1] === 1)
+                        return; // subscript 1002 success
+                    tickEvent(data[2]);
+                } else if (channel === 1010) {
+                    // heartbeat
+                } else if (self.marketChannel.indexOf(channel) !== -1) {
+                    tradeEvent(data[2], cp);
+                    // Trade Event
+                } else {
+                    if (data[2][0][0] === 'i') { // TradeInit
+                        self.marketChannel.push(channel);
+                        tradeInit(data[2][0][1].orderBook, cp);
+                    }
+                    // Trade init
+                } // end if
+
+
+            };
+
+            mySocket.onclose = function () {
+                console.log("Websocket connection closed");
+            };
+
+        };
+
+    };
+
+    //this.t = new tick();
+
+    this.start = function () {
+        this.ws = new socket();
+        this.ws.start();
+    };
+
+};
+
+
+p = poloniex();
+p.start();
+
 
 bot.on('message', function (event) {
     if (event.message.type = 'text') {
@@ -36,7 +211,21 @@ bot.on('message', function (event) {
         let action = msgs[0];
         if (action === '價格' || action === '$') {
             let currency = msgs[1];
-            getTickerData(currency).then(ticker => {
+            ticker = p.ws.tick['USDT_'+currency];
+            let string = '現在價格 : ' + ticker.price ;
+            string += '\n過去24H最高價 : ' + ticker.high ;
+            string += '\n過去24H最低價 : ' + ticker.low ;
+            string += '\n漲幅 : ' + ticker.change + '%';
+            console.log(string);
+            event.reply(string).then(function (data) {
+                // success
+                console.log('success sent message' + data);
+            }).catch(function (error) {
+                // error
+                console.log('error');
+            });
+            
+            /*getTickerData(currency).then(ticker => {
                 console.log('in22');
                 let string = '現在價格 : ' + ticker.price ;
                 string += '\n過去24H最高價 : ' + ticker.high ;
@@ -50,7 +239,7 @@ bot.on('message', function (event) {
                     // error
                     console.log('error');
                 });
-            })
+            })*/
         }
 
     }
