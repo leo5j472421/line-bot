d5c14a80b91aecc1b1285c348e28adc13c86716b
Leo <leo5j472421@gmail.com>
2018-03-14 13:44:23 +0800

    GJ

--- src/index.js
+++ src/index.js
@@ -78,13 +78,13 @@ function chatlog(event){
     let type = event.type;
     let sent = true;
     let userId = event.source.userId;
-    bot.getUserProfile(userId).then( userName => {
+    bot.getUserProfile(userId).then( profile => {
         console.log('yes id here');
         let sqlquery = 'INSERT INTO public.chatlog(\n' +
             '\tdate, message, sent, "time", type, "userId", "userName")\n' +
-            '\tVALUES (\'' + datetime[0] + '\', \''+event.message.text +'\', True, \'' + datetime[1] + '\', \''+type+'\', \''+userId+'\', \''+userName+'\');';
+            '\tVALUES (\'' + datetime[0] + '\', \''+event.message.text +'\', True, \'' + datetime[1] + '\', \''+type+'\', \''+userId+'\', \''+profile.displayName+'\');';
         console.log(sqlquery);
-        client.query(sqlquert, (err, res) => {
+        client.query(sqlquery, (err, res) => {
             if (err)
                 console.log(err);
             else
